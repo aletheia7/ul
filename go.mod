43018a3436361bad6a0e23bd95dc148182fd9e58
aletheia7 <aletheia7@users.noreply.github.com>
2022-03-18 16:46:10 -0700

    go.mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/aletheia7/ul
+
+go 1.17
--- z_test.go
+++ z_test.go
@@ -1,4 +1,4 @@
-// Copyright 2016 aletheia7. All rights reserved. Use of this source code is
+// Copyright 2022 aletheia7. All rights reserved. Use of this source code is
 // governed by a BSD-2-Clause license that can be found in the LICENSE file.
 
 package ul_test
